a4853447a5c115531d26fcc6f12be2a73b8b3e65
Stephen Johnston <52013605+stephendjohnston@users.noreply.github.com>
2023-04-13 12:11:30 -0300

    remove console logs

--- src/destroy.js
+++ src/destroy.js
@@ -3,19 +3,16 @@ import inquirer from "inquirer";
 import CONSTANTS from '../lib/constants.js';
 const { APP_NAME, COMMANDS, KEY_NAME } = CONSTANTS;
 
-const options = { stdio: "pipe" };
-
 export default async function destroy() {
   try {
     const confirm = await inquirer.prompt([
       {
         type: "confirm",
         name: "delete",
-        message: "Would you like to proceed with Herald's deletion?"
+        message: "Are you sure you want to delete Herald from AWS?"
       }
     ])
     if (confirm.delete) {
-      console.log(confirm)
       sh.cd(APP_NAME);
       sh.exec(COMMANDS.DESTROY);
       sh.exec(`${COMMANDS.DELETE_KEY} ${KEY_NAME}`);
